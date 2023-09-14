ac20b91920e957319e5d7d06e5f887693f049b5d
zadok <aduazadok@gmail.com>
2023-09-14 19:06:20 -0400

    I am not sure if this right. But I thought I will try to start something.

--- template-processor.js
+++ template-processor.js
@@ -1,1 +1,14 @@
 'use strict';
+class templateProcessor{
+    constructor(template) {
+        this.template = template;
+    }
+
+    fillIn = function (dictionary){
+        return template;
+    }
+
+
+
+
+}
