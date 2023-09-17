f4e95dc8f0c18395eeb3f2812a4bc13b6e5acbf6
Vulcran <104237476+Vulcran@users.noreply.github.com>
2023-09-17 14:10:48 -0400

    I Finished make-multi-filter.js and it functions in the file itself. When trying to run the function from test-project2.js I got an error

--- make-multi-filter.js
+++ make-multi-filter.js
@@ -1,29 +1,42 @@
 'use strict';
+//MakeMultiFilter function hold onto the values of the filter opperation
+//Also returns a function that can be called with different criteria
 function MakeMultiFilter(originalArray){
-    let currentArray = originalArray;
     function arrayFilterer(filterCriteria, callback){
-        if(filterCriteria==null){
-            return originalArray;
+
+        if(arrayFilterer.currentArray==null){
+            arrayFilterer.currentArray = Array.from(originalArray);
         }
-        console.log(originalArray);
-        for (let i = originalArray.length; i >= 0; i--){
-            if(!filterCriteria(currentArray[i])){
-                currentArray.splice(i,1);
+
+
+        if(filterCriteria==null){
+            return arrayFilterer.currentArray;
+        }
+
+        for (let i = arrayFilterer.currentArray.length; i >= 0; i--){
+            if(!filterCriteria(arrayFilterer.currentArray[i])){
+                arrayFilterer.currentArray.splice(i,1);
             }
-            console.log(currentArray);
         }
 
         if(callback!=null){
-            callback();
+            callback.call(originalArray, arrayFilterer.currentArray);
+            //callback(arrayFilterer.currentArray);
+            //callback.call();
+            //callback.apply("test");
         }
+        return arrayFilterer;
+
     }
+
     return arrayFilterer;
 }
+module.exports = MakeMultiFilter;
 
 // Invoking MakeMultiFilter() with originalArray = [1, 2, 3] returns a
 // function, saved in the variable arrayFilterer1, that can be used to
 // repeatedly filter the input array
-let arrayFilterer1 = MakeMultiFilter([1, 2, 3]);
+var arrayFilterer1 = MakeMultiFilter([1, 2, 3]);
 
 // Call arrayFilterer1 (with a callback function) to filter out all the numbers
 // not equal to 2.
@@ -35,8 +48,6 @@ arrayFilterer1(function (elem) {
     console.log(currentArray); // prints [1, 3]
 });
 
-
-/*
 // Call arrayFilterer1 (without a callback function) to filter out all the
 // elements not equal to 3.
 arrayFilterer1(function (elem) {
@@ -59,4 +70,3 @@ var arrayFilterer3 = MakeMultiFilter([1, 2, 3]);
 var arrayFilterer4 = MakeMultiFilter([4, 5, 6]);
 console.log(arrayFilterer3(filterTwos)()); // prints [1, 3]
 console.log(arrayFilterer4(filterThrees)()); // prints [4, 5, 6]
-*/
