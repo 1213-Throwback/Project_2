b40fef41ed14da9fa0f43de03958caa7be9458ae
Vulcran <104237476+Vulcran@users.noreply.github.com>
2023-09-17 19:57:55 -0400

    Commenting out old work which was not needed (intentional casting of module for Node Compatability)

--- make-multi-filter.js
+++ make-multi-filter.js
@@ -28,9 +28,10 @@ function MakeMultiFilter(originalArray){
         return arrayFilterer;
 
     }
-    if (typeof module === 'object') {
-        module.exports = MakeMultiFilter;
-    }// else{
+    // if (typeof module === 'object') {
+    //     module.exports = MakeMultiFilter;
+    // }
+    // else{
     //     module.id= 'function';
     // }
     return arrayFilterer;
@@ -39,7 +40,7 @@ function MakeMultiFilter(originalArray){
 // Invoking MakeMultiFilter() with originalArray = [1, 2, 3] returns a
 // function, saved in the variable arrayFilterer1, that can be used to
 // repeatedly filter the input array
-var arrayFilterer1 = MakeMultiFilter([1, 2, 3]);
+let arrayFilterer1 = MakeMultiFilter([1, 2, 3]);
 
 // Call arrayFilterer1 (with a callback function) to filter out all the numbers
 // not equal to 2.
@@ -58,18 +59,18 @@ arrayFilterer1(function (elem) {
 });
 
 // Calling arrayFilterer1 with no filterCriteria should return the currentArray.
-var currentArray = arrayFilterer1();
+let currentArray = arrayFilterer1();
 console.log('currentArray', currentArray); // prints [1] since we filtered out 2 and 3
 
 // Since arrayFilterer returns itself, calls can be chained
 function filterTwos(elem) { return elem !== 2; }
 function filterThrees(elem) { return elem !== 3; }
-var arrayFilterer2 = MakeMultiFilter([1, 2, 3]);
-var currentArray2 = arrayFilterer2(filterTwos)(filterThrees)();
+let arrayFilterer2 = MakeMultiFilter([1, 2, 3]);
+let currentArray2 = arrayFilterer2(filterTwos)(filterThrees)();
 console.log('currentArray2', currentArray2); // prints [1] since we filtered out 2 and 3
 
 // Multiple active filters at the same time
-var arrayFilterer3 = MakeMultiFilter([1, 2, 3]);
-var arrayFilterer4 = MakeMultiFilter([4, 5, 6]);
+let arrayFilterer3 = MakeMultiFilter([1, 2, 3]);
+let arrayFilterer4 = MakeMultiFilter([4, 5, 6]);
 console.log(arrayFilterer3(filterTwos)()); // prints [1, 3]
 console.log(arrayFilterer4(filterThrees)()); // prints [4, 5, 6]
